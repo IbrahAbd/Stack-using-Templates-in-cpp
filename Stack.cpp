a206a65c35267181205d060f5d46d357d8e94069
IbrahAbd <ibrahim.abdou2121@gmail.com>
2024-12-26 12:39:34 +0200

    Working stack program.

--- Stack.cpp
+++ Stack.cpp
@@ -19,36 +19,69 @@ class Stack{
         // Pop/remove  first element in stack.
         void pop(){
             if (!stack.empty()){
-                cout << stack[0] << "Popped.";
+                cout << stack[0] << " Popped." << endl;
                 stack.erase(stack.begin());
             }else{
-                cout << "Can't pop off an empty stack!";
+                cout << "Can't pop off an empty stack!" << endl;
             }
         }
 
         // Outputs a peek at the top element of the stack.
         void peek(){
-            cout << stack[0] << "is ontop of the stack!";
+            if (stack.empty()){
+                cout << "Stack is empty!" << endl;
+                return;
+            }
+            cout << "Top of the stack: " << stack[0] << endl;
         }
 
         void isEmpty(){
             if (stack.empty()){
-                cout << "Stack is empty!";
+                cout << "Stack is empty!" << endl;
             }
             else{
-                cout << "Stack is NOT empty";
+                cout << "Stack is NOT empty" << endl;
             }
         }
 }; 
 
 int main(void){
-    vector<int> stack1 = {1,2,3,4,5,6};
+    vector<int> stack1 = {};
     Stack<int> stack(stack1);
+    bool flag = true;
 
-    stack.push(9);
-    stack.peek();
-    stack.pop();
-    stack.pop();
+    while (flag){
+        cout << "1. Push" << endl;
+        cout << "2. Pop" << endl;
+        cout << "3. Peek" << endl;
+        cout << "4. Is Empty" << endl;
+        cout << "5. Exit" << endl;
+        int choice;
+        cin >> choice;
+        switch (choice){
+        case 1:
+            int value;
+            cout << "Enter value to push: ";
+            cin >> value;
+            stack.push(value);
+            break;
+        case 2:
+            stack.pop();
+            break;
+        case 3:
+            stack.peek();
+            break;
+        case 4:
+            stack.isEmpty();
+            break;
+        case 5: 
+            flag = false;
+            break;
+            
+        default:
+            break;
+        }
+    }
 
     return 0;
 }
